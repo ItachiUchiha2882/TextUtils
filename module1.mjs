4ef2f31c0bed20a97beeb2b5ab4c63b9c74ea6da
Sourabh Bhosale <93050571+sourabhb28@users.noreply.github.com>
2022-07-13 22:14:53 +0530

    completed tut6

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import titan,  {b, c, d} from "./module2.mjs";
+console.log(titan);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Eren";
+const b = "Mikasa";
+const c = "Armin";
+const d = "Levi";
+
+export default a;
+export {b};
+export {c};
+export {d};
